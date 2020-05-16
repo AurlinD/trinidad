4eba39e22976411cc2a816efb3df6eae1691245b
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-05-15 18:00:46 -0700

    created basic links to major pages in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,8 +4,14 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <nav>
-      <div>
+      <div className="left_menu">
         <Link to="/">Trinidad</Link>
+      </div>
+      <div className="right_menu">
+        <Link to="/about_us">About Us</Link>
+        <Link to="/what_we_do">What We Do</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/custom_order">Custom Order</Link>
       </div>
     </nav>
   );
