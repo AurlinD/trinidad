f851d2c9e572a17b27239e96cab740376513124e
Julian <julian@juliantomlindesign.com>
2020-05-18 22:42:36 +0000

    changed aboutUs page to work with components

---
+++ client/src/components/AboutUsBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class AboutUsBox extends Component {
+  render() {
+    return(
+    <div>
+      <h2> What Are Silicones </h2>
+      <p> Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis, dicit legendos at sit. Mea quando tritani ne, ne vix wisi labores. Singulis patrioque conclusionemque eu vim. Cum esse meis ut. Assueverit interpretaris ut usu, elit bonorum facilisi sed te. </p>
+    </div>
+    );
+  }
+}
