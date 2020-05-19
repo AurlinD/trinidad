36ab89667bdfc2cc0276595f2faa2572c09a1063
Julian <julian@juliantomlindesign.com>
2020-05-19 07:11:26 +0000

    about us grey headder background now overlaps picture

--- client/src/components/AboutUsBox.js
+++ client/src/components/AboutUsBox.js
@@ -7,7 +7,11 @@ export default class AboutUsBox extends Component {
     return(
     <div class={`about-us-box ${this.props.alignRight ? "reverse" : ""}`}>
       <div class="about-us-text">
-        <h2> What Are Silicones </h2>
+        <div class="about-us-headder-back">
+          <h2> What Are Silicones </h2>
+          <div class="about-us-grey-back">
+          </div>
+        </div>
         <p> Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis, dicit legendos at sit. Mea quando tritani ne, ne vix wisi labores. Singulis patrioque conclusionemque eu vim. Cum esse meis ut. Assueverit interpretaris ut usu, elit bonorum facilisi sed te. </p>
       </div>
       <img class="about-us-img" />
--- client/src/components/pages/AboutUs.js
+++ client/src/components/pages/AboutUs.js
@@ -6,13 +6,13 @@ export default function AboutUs() {
   return <div class="page">
 
   <AboutUsBox
-    alignRight={false}
-  />
-  <AboutUsBox
     alignRight={true}
   />
   <AboutUsBox
     alignRight={false}
+  />
+  <AboutUsBox
+    alignRight={true}
   />
   </div>
 }
