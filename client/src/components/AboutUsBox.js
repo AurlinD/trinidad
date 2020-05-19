8cd9ac40e8f419b3b50a048ce5d9b17332f62e5a
Julian <julian@juliantomlindesign.com>
2020-05-19 22:48:47 +0000

    Can now change the headder and body of each aboutUsBox component by changing paramaters in the aboutUs page

--- client/src/components/AboutUsBox.js
+++ client/src/components/AboutUsBox.js
@@ -8,11 +8,11 @@ export default class AboutUsBox extends Component {
     <div class={`about-us-box ${this.props.alignRight ? "reverse" : ""}`}>
       <div class="about-us-text">
         <div class="about-us-headder-back">
-          <h2> What Are Silicones </h2>
+          <h2> {this.props.aboutUsHeadder} </h2>
           <div class="about-us-grey-back">
           </div>
         </div>
-        <p> Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis, dicit legendos at sit. Mea quando tritani ne, ne vix wisi labores. Singulis patrioque conclusionemque eu vim. Cum esse meis ut. Assueverit interpretaris ut usu, elit bonorum facilisi sed te. </p>
+        <p> {this.props.aboutUsBody} </p>
       </div>
       <div class="about-us-img-box">
         <img class="about-us-img" src="/lib/test.png" />
--- client/src/components/pages/AboutUs.js
+++ client/src/components/pages/AboutUs.js
@@ -5,14 +5,23 @@ import AboutUsBox from "../AboutUsBox.js";
 export default function AboutUs() {
   return <div class="page">
 
+  //aboutUsHeadder will be the headder of the section
+  //aboutUsBody will be the body displayed in the section
+
   <AboutUsBox
     alignRight={true}
+    aboutUsHeadder="What are Silicones?"
+    aboutUsBody="Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis, dicit legendos at sit. Mea quando tritani ne, ne vix wisi labores. Singulis patrioque conclusionemque eu vim. Cum esse meis ut. Assueverit interpretaris ut usu, elit bonorum facilisi sed te."
   />
   <AboutUsBox
     alignRight={false}
+    aboutUsHeadder="Why Trinidad & Tobago?"
+    aboutUsBody="Access to sand and carbon, with easy access to shipping lanes"
   />
   <AboutUsBox
     alignRight={true}
+    aboutUsHeadder="Why Partner With Us?"
+    aboutUsBody="High quality product at competitive prices, Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis,"
   />
   </div>
 }
