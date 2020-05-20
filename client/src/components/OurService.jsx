47753377c9d25ba65a4d68b838541dcbe873710c
Julian <julian@juliantomlindesign.com>
2020-05-20 21:53:52 +0000

    added buttons to ourService component and styled them

---
+++ client/src/components/OurService.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import "../app.css"
+
+export default class OurService extends Component {
+  render() {
+    return(
+      <div class="our-service">
+        <div class="button-container">
+          <button type="button" class="our-service-button">Construction</button>
+          <button type="button" class="our-service-button">Medical & Surgical</button>
+          <button type="button" class="our-service-button">Manufacturing</button>
+          <button type="button" class="our-service-button">Aerospace & Marine</button>
+          <button type="button" class="our-service-button">Solar & Energy</button>
+          <button type="button" class="our-service-button">Personal Care & Beauty</button>
+          <button type="button" class="our-service-button">Electronics</button>
+          <button type="button" class="our-service-button">Custom Formulations</button>
+          <button type="button" class="our-service-button">Automotive</button>
+        </div>
+      </div>
+    )
+  }
+}
--- client/src/components/pages/AboutUs.js
+++ client/src/components/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import AboutUsBox from "../AboutUsBox.js";
+import OurService from "../OurService.jsx"
 
 export default function AboutUs() {
   return <div class="page">
@@ -9,19 +10,21 @@ export default function AboutUs() {
     alignRight={true}
     aboutUsHeader="What are Silicones?"
     aboutUsBody="Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis, dicit legendos at sit. Mea quando tritani ne, ne vix wisi labores. Singulis patrioque conclusionemque eu vim. Cum esse meis ut. Assueverit interpretaris ut usu, elit bonorum facilisi sed te."
-    imgName="test.png"
+    imgName="what.png"
   />
   <AboutUsBox
     alignRight={false}
     aboutUsHeader="Why Trinidad & Tobago?"
     aboutUsBody="Access to sand and carbon, with easy access to shipping lanes"
-    imgName="test.png"
+    imgName="trinidad.png"
   />
   <AboutUsBox
     alignRight={true}
     aboutUsHeader="Why Partner With Us?"
     aboutUsBody="High quality product at competitive prices, Lorem ipsum dolor sit amet, quo utinam feugiat no, pro ea tota propriae officiis,"
     imgName="test.png"
+  />
+  <OurService
   />
   </div>
 }
