d668a1cfebcdc3ff8d9045b4ccc13e4070cf43f2
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-06-25 17:54:04 -0700

    Updates

--- precache-manifest.f9c391419f3a815cd0f161d2d07bd595.js
+++ precache-manifest.f9c391419f3a815cd0f161d2d07bd595.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "28670e4af9e931bd5a13645f7bbbaf33",
+    "revision": "9b0535eb8627249f0c31d5f4446b93e7",
     "url": "/trinidad/index.html"
   },
   {
-    "revision": "bf1bb39d93c5bf49daf9",
+    "revision": "529f884e17f4d158f89e",
     "url": "/trinidad/static/css/main.eaebcc91.chunk.css"
   },
   {
-    "revision": "5e2d25b89e65694766c0",
-    "url": "/trinidad/static/js/2.8018cd9e.chunk.js"
+    "revision": "4d4ff1acdf54aff78617",
+    "url": "/trinidad/static/js/2.90dec130.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/trinidad/static/js/2.8018cd9e.chunk.js.LICENSE.txt"
+    "url": "/trinidad/static/js/2.90dec130.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bf1bb39d93c5bf49daf9",
-    "url": "/trinidad/static/js/main.2449ce37.chunk.js"
+    "revision": "529f884e17f4d158f89e",
+    "url": "/trinidad/static/js/main.a2279586.chunk.js"
   },
   {
     "revision": "6860da1d2aff3e29bf12",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trinidad/precache-manifest.7c06a98df5d5dce63e1f8959e6bc45dd.js"
+  "/trinidad/precache-manifest.f9c391419f3a815cd0f161d2d07bd595.js"
 );
 
 self.addEventListener('message', (event) => {
